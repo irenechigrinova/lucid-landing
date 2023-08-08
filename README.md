522aeca18c4bdfb27e6b18e46e641957ada4be9e
Irina Chigrinova <to.chigrinova@gmail.com>
2023-08-08 14:15:18 +0300

    readme added

---
+++ README.md
@@ -0,0 +1,3 @@
+Refactored lucidnode.com with [gsap](https://greensock.com/gsap/)
+
+[DEMO](https://irenechigrinova.github.io/lucid-landing/)
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/lucid-landing/",
+  build: {
+    assetsDir: "",
+  },
 });
